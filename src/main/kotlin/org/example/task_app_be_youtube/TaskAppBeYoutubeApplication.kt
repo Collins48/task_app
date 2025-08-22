d4456496c7ac21b07ac44b2bf8d20b611f9f3a36
Collins48 <collinkimutai648@gmail.com>
2025-08-22 15:58:39 +0300

    Initial commit

---
+++ src/main/kotlin/org/example/task_app_be_youtube/TaskAppBeYoutubeApplication.kt
@@ -0,0 +1,12 @@
+package org.example.task_app_be_youtube
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskAppBeYoutubeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskAppBeYoutubeApplication>(*args)
+    println("Marahaba ya shabab")
+}
