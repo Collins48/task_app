51766bfd4a605f60a27ec82cc5c7183a2f315482
Collins48 <collinkimutai648@gmail.com>
2025-09-01 21:46:58 +0300

    service

--- src/main/kotlin/org/example/task_app_be_youtube/controller/TaskController.kt
+++ src/main/kotlin/org/example/task_app_be_youtube/controller/TaskController.kt
@@ -1,11 +1,18 @@
 package org.example.task_app_be_youtube.controller
 
+import jakarta.validation.Valid
+import org.example.task_app_be_youtube.data.model.TaskCreateRequest
 import org.example.task_app_be_youtube.data.model.TaskDto
+import org.example.task_app_be_youtube.data.model.TaskUpdateRequest
 import org.example.task_app_be_youtube.service.TaskService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -32,6 +39,20 @@ class TaskController(private val service: TaskService) {
     @GetMapping("tasks/{id}")
     fun getTaskById(@PathVariable id: Long): ResponseEntity<TaskDto> =
         ResponseEntity(service.getTaskById(id), HttpStatus.OK)
+
+    @PostMapping("create")
+    fun createTask(
+        @Valid @RequestBody request: TaskCreateRequest
+    ): ResponseEntity<TaskDto> = ResponseEntity(service.createTask(request), HttpStatus.OK)
+
+    @PatchMapping("update/{id}")
+    fun updateTask(
+        @PathVariable id: Long,
+        @Valid @RequestBody request: TaskUpdateRequest
+    ):ResponseEntity<TaskDto> = ResponseEntity(service.updateTask(id, request), HttpStatus.OK)
+
+    @DeleteMapping("delete/{id}")
+    fun deleteTask(@PathVariable id: Long): ResponseEntity<String> = ResponseEntity(service.deleteTask(id), HttpStatus.OK)
 
 
 }
---
+++ src/main/kotlin/org/example/task_app_be_youtube/cors/CorsConfig.kt
@@ -0,0 +1,26 @@
+package org.example.task_app_be_youtube.cors
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class CorsConfig {
+
+
+    @Bean
+    fun getCorsConfiguration(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry){
+
+                registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+            }
+        }
+    }
+}
